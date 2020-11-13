f9d1777ace2ec427d187a31c9544a3f49a27b0a9
apatil987 <56457146+apatil987@users.noreply.github.com>
2020-11-12 17:01:53 -0800

    Update player.py

--- player.py
+++ player.py
@@ -1,8 +1,5 @@
 import streamlit as st
 import pandas as pd
-import base64
-import matplotlib.pyplot as plt
-import seaborn as sns
 import numpy as np
 import SessionState
 state = SessionState.get(searches=[])
